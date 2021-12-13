c4253f4db12621db350a6bcfa0f65ab8cb4f587c
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2021-12-13 18:11:02 +0900

    update: おそらく A-Frame のバージョンアップで不要となったコードを削除

--- docs/aframe-objective-component.js
+++ docs/aframe-objective-component.js
@@ -13,17 +13,6 @@ AFRAME.registerComponent('objective', {
 
 		cameraEl.removeAttribute('wasd-controls');
 
-		// A-Frame バグ対策
-		cameraEl.sceneEl.addEventListener('exit-vr', () => {
-
-			// Issue: https://github.com/aframevr/aframe/issues/3401#issuecomment-370119459
-			cameraEl.object3D.position.set(0, 1.6, 0); // DEFAULT_CAMERA_HEIGHT: 1.6
-
-			// Issue: https://github.com/aframevr/aframe/issues/3884
-			cameraEl.object3D.rotation.z = 0;
-
-		});
-
 	},
 
 	tick(time, timeDelta) {
@@ -40,10 +29,6 @@ AFRAME.registerComponent('objective', {
 		vector.applyQuaternion(quaternion);
 		rigObject3D.position.copy(vector);
 		rigObject3D.position.sub(cameraObject3D.position);
-
-		// update
-		// メモ: これがないと次のフレームまで位置が正しく反映されない
-		cameraObject3D.updateMatrix();
 
 	},
 
